0e2ed30723902dd5a337e07f94c31ba1727811da
Geoff Rowland <rowland.geoff@gmail.com>
2025-11-23 22:18:05 +0000

    Update L.TileLayer.BetterWMS.js

--- L.TileLayer.BetterWMS.js
+++ L.TileLayer.BetterWMS.js
@@ -205,8 +205,8 @@ L.TileLayer.BetterWMS = L.TileLayer.WMS.extend({
         period2 = bedrock[1].split(";")[61];
         if (period1 == period2) {period = period1} else {period = period1 + ' to ' + period2};
         name = bedrock[1].split(";")[66];
-		formation = bedrock[1].split(";")[74];
-		group = bedrock[1].split(";")[76];
+		//formation = bedrock[1].split(";")[74];
+		//group = bedrock[1].split(";")[76];
         type = bedrock[1].split(";")[62];
         if (type != '') {
 		type = type[0].toUpperCase() + type.slice(1);
@@ -371,8 +371,8 @@ L.TileLayer.BetterWMS = L.TileLayer.WMS.extend({
 //	if (name == '') {name = ''; age = ''; per if (name != '') {name = 'Bedrock: ' + name};iod = ''};
 //	    
     if (name != '') {name = 'Bedrock: ' + name};
-	if (formation != '') {formation = '<br/>Formation: ' + formarion};
-	if (group != '') {group = '<br/>Group: ' + group};
+	//if (formation != '') {formation = '<br/>Formation: ' + formarion};
+	//if (group != '') {group = '<br/>Group: ' + group};
 	if (age != '') {age = '<br/>Age: ' + age}; 
 	if (period != '') {period = '<br/>Period: ' + period}; 
 	desc = bedrock[1].split(";")[63];
@@ -389,7 +389,7 @@ L.TileLayer.BetterWMS = L.TileLayer.WMS.extend({
         if (sett2 == 'Null'){sett2 = ''};
         detail = bedrock[1].split(";")[85];
         if (detail != '') {detail = '<br/>Detail: ' + detail};
-        bedrockcontent =  name + formation + group + age + period + type + desc + sett1 + ' ' + sett2 + detail + '<br/><br/>Locaton: ' + bngString + '<br/>';
+        bedrockcontent =  name + age + period + type + desc + sett1 + ' ' + sett2 + detail + '<br/><br/>Locaton: ' + bngString + '<br/>';
      };
     linearcontent = '';
     linear = content.split('@BGS.50k.Linear.features');
